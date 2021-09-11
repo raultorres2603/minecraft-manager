61abd4b0b2e70a9f87d623c483f55cd10d9f6dcf
Raul <raultorres2603@gmail.com>
2021-09-11 02:23:03 +0200

    Error management and check version

--- src/app.js
+++ src/app.js
@@ -1,56 +1,119 @@
-const { app, BrowserWindow, ipcMain, Menu, WebContents,globalShortcut } = require('electron')
+const { app, BrowserWindow, ipcMain, Menu, globalShortcut, webContents } = require('electron')
 const path = require('path')
 const fs = require('fs');
 const os = require('os');
+const url = require('url');
+const { exec, spawn } = require('child_process');
 
 require('electron-reload')(__dirname, {
-    electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
-  });
+  electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
+});
 
-var mainWindow
+var mainWindow;
+var versiones = require("./public/versiones/versiones.json");
+var versionWindow;
+var currentVersion;
 
 ipcMain.on('cerrarApp', (event, args) => {
-    if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') app.quit()
 })
 
+ipcMain.on('volverAlMenu', () => {
+  mainWindow.loadFile(__dirname + "/../views/menu.html");
+})
+
+ipcMain.on('comprobarVersionError', (event, args) => {
+  createErrorVersion()
+})
+
+ipcMain.on('cerrarError', (event, args) => {
+  versionWindow.close()
+})
+
+ipcMain.on('comprobarVersion', (event, args) => {
+  let version = args[0];
+  fs.readdir(path.join(os.homedir(), "AppData", "Roaming", ".minecraft", "versions", version), (err, files) => {
+    if (err) {
+      event.sender.send('comprobarVersion_ok', ["error", version])
+    } else {
+      console.log(files);
+    }
+  })
+})
+
+ipcMain.on('elegirVersion', (event, args) => {
+  changeVersionWindow()
+})
+
+function createErrorVersion() {
+  versionWindow = new BrowserWindow({
+    width: 800,
+    height: 200,
+    darkTheme: true,
+    icon: "./src/public/img/icono.png",
+    center: true,
+    frame: false,
+    resizable: false,
+    title: "Error!",
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+
+  versionWindow.loadFile(__dirname + "/../views/errorVersion.html")
+}
+
 function createMain() {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        show: false,
-        darkTheme: true,
-        icon: "./src/public/img/icono.png",
-        center: true,
-        frame: false,
-        resizable: false,
-        title: "Minecraft Manager",
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-          },
-    })
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show: false,
+    darkTheme: true,
+    icon: "./src/public/img/icono.png",
+    center: true,
+    frame: false,
+    resizable: false,
+    title: "Minecraft Manager",
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
 
-    mainWindow.on('closed', () => {
-        if (process.platform !== 'darwin') app.quit()
-    })
+  mainWindow.on('closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
 
-    mainWindow.once('ready-to-show', () => {
-        mainWindow.show()
-      })
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
 
-    mainWindow.setMenu(null);
+  mainWindow.setMenu(null);
 
-    mainWindow.loadFile(__dirname + "/../views/menu.html");
+  mainWindow.loadFile(__dirname + "/../views/menu.html");
+}
+
+function changeVersionWindow() {
+  mainWindow.loadFile(__dirname + "/../views/elegirVersion.html")
+
+  setTimeout(() => {
+    mainWindow.webContents.send('mandarVersiones', [versiones])
+  }, 100);
+
+
 }
 
 app.whenReady().then(() => {
-    createMain();
+  createMain();
 
-    globalShortcut.register('Ctrl+Shift+I', () => {
-        mainWindow.webContents.toggleDevTools();
-      })
+  globalShortcut.register('Ctrl+Shift+I', () => {
+    mainWindow.webContents.toggleDevTools();
   })
 
-  app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
-  })
+
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
--- src/public/renderer.js
+++ src/public/renderer.js
@@ -1,5 +1,46 @@
-const { ipcRenderer} = require('electron');
+const { ipcRenderer, BrowserWindow, BrowserView, webContents } = require('electron');
 
 function cerrarApp() {
     ipcRenderer.send('cerrarApp');
+}
+
+ipcRenderer.on('mandarVersiones', (event, args) => {
+    let versiones = args[0]
+    let select = document.getElementById('selectVersiones');
+    select.append(new Option("Elige una version", "Elige una version"))
+    versiones.forEach((version, posVersion) => {
+        select.append(new Option(version.version, version.version))
+    });
+})
+
+ipcRenderer.on('comprobarVersion_ok', (event, args) => {
+    let codigo = args[0];
+    let version = args[1];
+
+    switch (codigo) {
+        case "error":
+            ipcRenderer.send('comprobarVersionError', [version])
+            break;
+
+        default:
+            break;
+    }
+})
+
+function elegirVersion() {
+    ipcRenderer.send('elegirVersion')
+}
+
+function cerrarError() {
+    ipcRenderer.send('cerrarError')
+}
+
+function aceptarVersion(version) {
+    if (version != "Elige una version") {
+        ipcRenderer.send('comprobarVersion', [version])
+    }
+}
+
+function volverAlMenu() {
+    ipcRenderer.send('volverAlMenu')
 }
